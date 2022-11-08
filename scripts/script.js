a59b13839b6116992b86833e0c7d39346cee92ab
Louie <causingljbcit@gmail.com>
2022-11-08 10:06:18 -0800

    for loop and while loop

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+// alert("Your computer has been infected by virus")
+// console.log("Download our free antivirus to help disenfect")
+
+// power = [1,2,3,4,5,6,7,8,9,10]
+
+// for loop
+for (i=1 ;i <= 10;i++){
+console.log(Math.pow(2, i))
+}
+
+// While loop
+let i = 1
+while(i<=10)
+{
+    console.log(Math.pow(2,i))
+    i++
+}
